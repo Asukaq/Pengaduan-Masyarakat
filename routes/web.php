7e86df09a3463780438eaa0fb03a431e006f3ef3
Akane <75875762+Asukaq@users.noreply.github.com>
2024-06-02 23:28:48 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/category/create', '\App\Http\Controllers\CategoryController@create');
+Route::get('/category/index', '\App\Http\Controllers\CategoryController@index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
---
+++ storage/framework/views/170460ded64c18e268718f6b9ac3fe3f.php
@@ -0,0 +1,15 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Manage Food Category</div>
+                </div class="card-body">
+                    Ini Category
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\sudar\login_laravel\resources\views/category/create.blade.php ENDPATH**/ ?>
